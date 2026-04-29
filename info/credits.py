065a431bf6bb965b18f7107bf12d896fd4aeda0f
NotDaGoober <benjaminlevikrahn@gmail.com>
2026-04-28 19:35:06 -0500

    Add files via upload

---
+++ info/credits.py
@@ -0,0 +1,5 @@
+# Running !!credits or !!creds or any specific debug command in The Sandbox will bring you here.
+print("[-] The Velocity Coding Language is 100% owned by NotDaBenjamin")
+print("[-] Credit is required if you want to feature this project.")
+print("\n[-] These are the ONLY official websites:\nhttps://notdabenjamin.neocities.org (or the other alternates found on the sitemap)\nhttps://learnvelocity.pages.dev\nhttps://github.com/LearnVelocity\nhttps://notdb.pages.dev (For Link Shortening)\n\nQuestions: Email me at notdabenjamin@gmail.com or by clicking on this link! https://notdb.pages.dev/c")
+print("\n\n[-] AI:\nLittle amounts of AI were used to help us fix bugs and teach us things to use in the code. All of the sections of code that were generated with AI have been edited by humans.")
---
+++ info/v.py
@@ -0,0 +1,2 @@
+print("[-] Version 1.0.0 CLOSED BETA\n\nUnmodified, Verified, open-source on Github: https://github.com/LearnVelocity/Velocity")
+print("[-] Run credits.py to learn more about the creator!")
